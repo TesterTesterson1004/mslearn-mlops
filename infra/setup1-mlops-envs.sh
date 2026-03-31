53c68c95d07f453ead37eb48a3d9693bf00278ff
TesterTesterson1004 <49700217+TesterTesterson1004@users.noreply.github.com>
2026-03-31 14:35:46 -0700

    Update setup1-mlops-envs.sh

--- infra/setup1-mlops-envs.sh
+++ infra/setup1-mlops-envs.sh
@@ -13,7 +13,7 @@ fi
 echo "Suffix: $suffix"
 
 RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
-REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
+REGIONS=("eastus" "westus" "northeurope" "westeurope")
 RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
 
 # Dev environment
